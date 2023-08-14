845db309661f582c78ddbe842f812e702531415a
mbizimis <mbizimis@atypon.com>
2023-08-14 13:51:00 +0300

    another bug

--- data_pipeline/src/gcp_utils.py
+++ data_pipeline/src/gcp_utils.py
@@ -5,7 +5,7 @@ def upload_file(oldfilepath: str, gspath: str, newfilename: str):
     """
     Uploads a generic file (e.g. that cant be saved with pandas) to GCP.
     """
-    bucket_name, bucket_path = gspath.split('/')[2], bucket_path='/'.join(gspath.split('/')[3:])
+    bucket_name, bucket_path = gspath.split('/')[2], '/'.join(gspath.split('/')[3:])
     client = storage.Client()
     bucket = client.get_bucket(bucket_name)
     blob = bucket.blob(bucket_path + f'/{newfilename}')
