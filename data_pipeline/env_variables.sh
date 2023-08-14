42e5db4072e232bb1ba45ec4d59feba8022b6f18
mbizimis <mbizimis@atypon.com>
2023-08-14 13:32:03 +0300

    Oops

--- data_pipeline/env_variables.sh
+++ data_pipeline/env_variables.sh
@@ -1,6 +1,5 @@
 export PROJECT_ID=$(gcloud config list project --format "value(core.project)")
-export REPO_NAME=vertex-aigcloud auth configure-docker \
-    us-central1-docker.pkg.dev-test
+export REPO_NAME=vertex-ai-test
 export IMAGE_NAME=data-pipeline
 export IMAGE_TAG=latest
 export IMAGE_URI=us-central1-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${IMAGE_NAME}:${IMAGE_TAG}
