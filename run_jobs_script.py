3c8037d4a8e0f0ca2655f3407263da7e3d99e74f
mbizimis <mbizimis@atypon.com>
2023-08-17 12:01:12 +0300

    Save trained model

--- run_jobs_script.py
+++ run_jobs_script.py
@@ -6,11 +6,25 @@ if __name__ == '__main__':
 
     job = aiplatform.CustomContainerTrainingJob(
         display_name="data-pipeline-remote",
-        container_uri="us-central1-docker.pkg.dev/information-discovery/vertex-ai-test/data-pipeline@sha256:8f6b11757899d2e8315011a9cd9de60c9b3b084f3d6f737a82f5e8f5148468ad",
+        container_uri="us-central1-docker.pkg.dev/information-discovery/vertex-ai-test/data-pipeline:latest",
         staging_bucket="ai_repo/VertextAI_test/staging_test",
     )
 
-    print('Running job...')
+    print('Running data job...')
+    res = job.run(
+        replica_count=1,
+        machine_type='n1-standard-4',
+        args=[]
+    )
+    print(f'Done:\n{res}')
+
+    job = aiplatform.CustomContainerTrainingJob(
+        display_name="train-pipeline-remote",
+        container_uri="us-central1-docker.pkg.dev/information-discovery/vertex-ai-test/train-pipeline:latest",
+        staging_bucket="ai_repo/VertextAI_test/staging_test",
+    )
+
+    print('Running train job...')
     res = job.run(
         replica_count=1,
         machine_type='n1-standard-4',
--- train_pipeline/train.py
+++ train_pipeline/train.py
@@ -1,13 +1,17 @@
-import numpy as np
 import pandas as pd
 from sklearn import linear_model
 from sklearn.metrics import mean_squared_error
+import os
+import joblib
+from google.cloud import storage
 
 
 gspath = 'gs://ai_repo/VertextAI_test'
 
 
 if __name__ == '__main__':
+    # TODO: for yaml config, have a local file changed and it should be uploaded on GCP path before every run to be read from there
+
     # read data
     X_train, X_test, y_train, y_test = [
         pd.read_json(f'{gspath}/{f}') for f in
@@ -23,3 +27,13 @@ if __name__ == '__main__':
 
     # log experiment TODO: mlflow
     print(f'{mse=}')
+
+    # Save model artifact to local filesystem (doesn't persist)
+    filename = "dummy_linear_model.jlb"
+    joblib.dump(model, filename)
+
+    # Upload model artifact to Cloud Storage
+    model_directory = os.environ['AIP_MODEL_DIR']
+    storage_path = os.path.join(model_directory, filename)
+    blob = storage.blob.Blob.from_string(storage_path, client=storage.Client())
+    blob.upload_from_filename(filename)
